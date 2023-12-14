68bfe5568caa1849a157a42734a79a54836e3752
LHKN <79393909+LHKN@users.noreply.github.com>
2023-12-14 18:23:41 +0700

    fix: update student id in profile

--- src/users/dto/update-profile.dto.ts
+++ src/users/dto/update-profile.dto.ts
@@ -1,11 +1,16 @@
-import { IsEmail, IsNotEmpty, MaxLength, Validate } from 'class-validator';
+import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator';
 
 export class UpdateProfileDto {
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
+  // @IsNotEmpty()
+  // @IsEmail()
+  // email: string;
 
   @IsNotEmpty()
   @MaxLength(16)
   username: string;
+
+  // imageUrl: string;
+
+  @MaxLength(16)
+  studentId: string;
 }
